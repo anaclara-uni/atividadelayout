56eafafea08f0d2d753e6cd1c927d6a7b284963d
anaclara-uni <272368@unifio.edu.br>
2025-09-11 23:51:04 +0000

    11/09/2025

--- public/index.php
+++ public/index.php
@@ -1,17 +1,22 @@
-<?php
-require '../vendor/autoload.php';
 
-use Application\Hello;
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link href="/css/default.css" rel="stylesheet" />
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+     <title>Larika</title>
+    <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/css/default.css" rel="stylesheet" />     
 </head>
 <body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#"> 
+    <img src="./img/ham-ico-64.png" alt="" width="32" height="32"> 
+    Larika 
+    </a>
+    </div>
+</nav> 
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" /></script>
 </body>
 </html>
